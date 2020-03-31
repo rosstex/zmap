d7e04862b0e61f154044badf9e807ef9f8bbf1be
Ross Teixeira <rapt@cs.princeton.edu>
2020-03-31 18:21:40 +0000

    typo

--- run_zmap.sh
+++ run_zmap.sh
@@ -5,5 +5,5 @@ wait
 case $1 in
   4) sudo zmap -p 20023 -M $2 --whitelist-file=ipv4.txt --source-ip=1.1.1.1 --probe-args=$3;;
   6) sudo zmap -p 20023 -M $2 --ipv6-target-file=ipv6.txt --ipv6-source-ip=fe80::1e69:7aff:fe60:7aa5 --probe-args=$3;;
-  *) echo "Please begin your command with '4' or '6'" && exit;
+  *) echo "Please begin your command with '4' or '6'" && exit;;
 esac
